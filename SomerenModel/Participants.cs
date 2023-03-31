fda8e2db721afad1e46599e6868109efceaa107d
Miguel-Kuin <113019405+Miguel-Kuin@users.noreply.github.com>
2023-03-31 13:53:12 +0200

    Ik ben nog bezig net ff een nieuwe dao gemaakt

---
+++ SomerenModel/Participants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Participants
+    {
+        public int ActivityId { get; set; }
+        public string Activity { get; set; }
+       
+    }
+}
---
+++ SomerenService/ParticipantsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SomerenDAL;
+using SomerenModel;
+
+namespace SomerenService
+{
+    public class ParticipantsService
+    {
+        private ParticipantsDao participantsdb;
+
+        public ParticipantsService()
+        {
+            participantsdb = new();
+        }
+
+        public List<Participants> GetParticipants()
+        {
+            List<Participants> participants = participantsdb.GetAllParticipants();
+            return participants;
+        }
+    }
+}
